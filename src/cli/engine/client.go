78cb9f0c5e4713ea8263b8072d7674d8ee4fa942
SpaceHead1C <damh17@gmail.com>
2020-03-01 21:15:39 +0300

    add channels for chatting

--- src/cli/engine/client.go
+++ src/cli/engine/client.go
@@ -6,11 +6,21 @@ import (
 
 // Client model
 type Client struct {
-	ws   *websocket.Conn
-	name string
+	ws        *websocket.Conn
+	name      string
+	sendCh    chan string
+	receiveCh chan *Message
 }
 
 // NewClient is a constructor
 func NewClient(ws *websocket.Conn, name string) *Client {
-	return &Client{ws, name}
+	sendCh := make(chan string)
+	receiveCh := make(chan *Message)
+
+	return &Client{
+		ws,
+		name,
+		sendCh,
+		receiveCh,
+	}
 }
