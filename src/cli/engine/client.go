ccbfa8d6911563f11118309da3f7fb5e6be938a1
SpaceHead1C <damh17@gmail.com>
2020-03-01 21:31:09 +0300

    add Say and Listen methods for chatting

--- src/cli/engine/client.go
+++ src/cli/engine/client.go
@@ -1,6 +1,10 @@
 package engine
 
 import (
+	"fmt"
+	"io"
+	"log"
+
 	"golang.org/x/net/websocket"
 )
 
@@ -22,5 +26,44 @@ func NewClient(ws *websocket.Conn, name string) *Client {
 		name,
 		sendCh,
 		receiveCh,
+	}
+}
+
+// Say some text
+func (c *Client) Say(text string) {
+	c.sendCh <- text
+}
+
+// Listen "write" and "read" events
+func (c *Client) Listen() {
+	go c.listenWrite()
+	c.listenRead()
+}
+
+func (c *Client) listenWrite() {
+	for {
+		select {
+		case text := <-c.sendCh:
+			websocket.JSON.Send(c.ws, &Message{c.name, text})
+		}
+	}
+}
+
+func (c *Client) listenRead() {
+	for {
+		select {
+		default:
+			var msg Message
+			err := websocket.JSON.Receive(c.ws, &msg)
+			if err == io.EOF {
+				log.Panicln("Empty message")
+			} else if err != nil {
+				log.Panicln(err)
+			} else {
+				if msg.Author != c.name {
+					fmt.Println(msg.String())
+				}
+			}
+		}
 	}
 }
